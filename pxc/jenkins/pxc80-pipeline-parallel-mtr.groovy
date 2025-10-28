b7842f046bb165aac270e46654626209642b3f10
Kamil Holubicki <kamil.holubicki@percona.com>
2025-10-28 14:47:58 +0100

    Initial version of parallel pipeline for pxc 5.7

--- pxc/jenkins/pxc80-pipeline-parallel-mtr.groovy
+++ pxc/jenkins/pxc80-pipeline-parallel-mtr.groovy
@@ -1,5 +1,5 @@
 PIPELINE_TIMEOUT = 24
-JENKINS_SCRIPTS_BRANCH = 'innovative'
+JENKINS_SCRIPTS_BRANCH = 'pxc-5.7'
 JENKINS_SCRIPTS_REPO = 'https://github.com/kamil-holubicki/jenkins-pipelines'
 AWS_CREDENTIALS_ID = 'c42456e5-c28d-4962-b32c-b75d161bff27'
 MAX_S3_RETRIES = 12
